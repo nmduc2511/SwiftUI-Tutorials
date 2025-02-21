2fb8a716a8611d9ede38053243232e18cfe8bcb6
Duc Nguyen <>
2025-02-21 19:57:43 +0700

    init project

---
+++ SwiftUI-Tutorials/SwiftUI-Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI-Tutorials
+//
+//  Created by Admin on 21/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
